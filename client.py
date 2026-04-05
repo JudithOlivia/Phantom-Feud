e2bc5f9192eb9a45960dc8c6e27be95acf1aa98a
JudithOlivia <jxdith13@gmail.com>
2026-04-04 23:18:51 -0400

    local player input  & animation

---
+++ client.py
@@ -0,0 +1,41 @@
+import pygame 
+import json
+import os
+import sys
+import threading
+
+pygame.init()
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+FPS = 60
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 100, 255)
+GRAY = (100, 100, 100)
+
+class PhantomFeudClient:
+    def __init__(self, server_ip="127.0.0.1", server_port=5555):
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        
+        pygame.display.set_caption("Phantom Feud")
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+        self.players = {}
+        self.other_players = {}
+        
+        self.local_x = 400
+        self.local_y = 400
+        self.local_direction = "down"
+        self.local_health = 100
+        self.local_max_health
+        self.local_action = "idle"
+        self.local_attack_timer = 0
+        
+        self.character_animations = {}
+        self.current_animation_frame = 0
+        self.animation_speed = 0.15
+        self.last_animation_update = 0
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket 
 import threading 
+import json
 import time
 from collections import OrderedDict
 
